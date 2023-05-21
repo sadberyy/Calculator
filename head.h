e59e538cea185a552e33caa8e1dbf9ea8e945e4b
MyNameIsGustavo <126723647+sadberyy@users.noreply.github.com>
2023-05-21 23:18:06 +0300

    Add files via upload

---
+++ head.h
@@ -0,0 +1,11 @@
+#pragma once
+#define max_stack_len 20
+#define max_exp_len 200
+#define max_word_len 15
+
+void convert(char inp[], char* out, int* size);
+void check(char num[]);
+void antiTrash(char num[]);
+void change_val(char num1[], char num2[], char symb);
+void clearCurr(char* str, int len);
+void calc(char out[], double* res, int size);
