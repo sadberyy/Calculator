82a389ba22a967fa709ee3582c098ce52628387b
MyNameIsGustavo <126723647+sadberyy@users.noreply.github.com>
2023-06-03 11:47:20 +0300

    -

--- head.h
+++ head.h
@@ -3,10 +3,11 @@
 #define max_exp_len 200
 #define max_word_len 15
 
-void convert(char inp[], char* out, int* size);
-void check(char num[]);
+void convert(char inp[], char* out, int* size, int* count_v);
+void check(char var[], char num[], int* count);
+void isE(char num[], double* now, int* flag);
 void antiTrash(char num[]);
 void change_val(char num1[], char num2[], char symb);
 void clearCurr(char* str, int len);
 void cleanNull(char num[]);
-void calc(char out[], double* res, int size);
+void calc(char out[], double* res, int size, int count_v, char var[]);
